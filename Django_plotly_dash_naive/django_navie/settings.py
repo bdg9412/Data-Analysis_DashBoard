ea937fc4e1e3e43490e786b3f838949f06242b90
DongKeun Bak <46730741+bdg9412@users.noreply.github.com>
2021-11-13 07:52:52 +0900

    Update settings.py

--- Django_plotly_dash_naive/django_navie/settings.py
+++ Django_plotly_dash_naive/django_navie/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kgtc5p0ot-t2m03(c%d)sj^kkj7nrbw9pj()a(+dae_$ej1-t4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
